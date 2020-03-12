0940b1a5080e9ad009e5995dd7b13de051983db5
Benoit Pasquier <briochemc@gmail.com>
2020-03-12 17:19:13 +1100

    try to test the 1min version too through CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,11 @@ using Test
 ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 
 @testset "Earth2014.jl" begin
-    x,y,z = Earth2014.load()
-    @test x isa Vector
-    @test y isa Vector
-    @test z isa Array
-    @test size(z) == (length(y), length(x))
+    @testset "$res resolution" for res in ["1min", "5min"]
+        x,y,z = Earth2014.load(res=res)
+        @test x isa Vector
+        @test y isa Vector
+        @test z isa Array
+        @test size(z) == (length(y), length(x))
+    end
 end
