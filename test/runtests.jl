9dc93a086666670e813a998609eecbb68c27b23a
Benoit Pasquier <briochemc@gmail.com>
2020-03-12 17:03:12 +1100

    Fix CI DataDeps always accept

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Earth2014
 using Test
 
+# For CI, make sure the downloads do not hang
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+
 @testset "Earth2014.jl" begin
     x,y,z = Earth2014.load()
     @test x isa Vector
